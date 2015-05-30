7cec2da71acb3d9819d7c654a5390320c922032e
Francois Perrad <francois.perrad@gadz.org>
2015-05-30 16:17:04 +0200

    prepare release 0.3.3

--- src/MessagePack.lua
+++ src/MessagePack.lua
@@ -1117,7 +1117,7 @@ else
 end
 set_array'without_hole'
 
-m._VERSION = "0.3.2"
+m._VERSION = '0.3.3'
 m._DESCRIPTION = "lua-MessagePack : a pure Lua implementation"
 m._COPYRIGHT = "Copyright (c) 2012-2015 Francois Perrad"
 return m
